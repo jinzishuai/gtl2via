3d951c41d0e436c7cd8d0055bd468eb6e5630cc2
Shi Jin <jinzishuai@gmail.com>
2020-08-22 13:54:02 -0600

    rename and fix testConverters.sh

--- test/testConverters.sh
+++ test/testConverters.sh
@@ -12,8 +12,7 @@ pushd $TOPFOLDER
 python -m gtl2via -i test/defect_boxes.manifest -o test/via.json -l zerobox-quick-detection -s zerobox-public
 
 if [[ $(cat test/via.json |wc -m) == 4403 ]]; then
-    echo "Correct results"
-    exit 0
+    echo "Correct results with gtl2via. Continue"
 else
     echo "Wrong results:"    
     cat test/via.json | jq
@@ -23,7 +22,7 @@ fi
 python -m viapl2rect -i test/polyline.json -o test/rect.json
 
 if [[ $(cat test/rect.json |jq |grep rect|wc -l) == 2 ]]; then
-    echo "Correct results"
+    echo "Correct results with viapl2rect"
     exit 0
 else
     echo "Wrong results: expected to find two rect"    

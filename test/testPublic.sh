4a92087fadbc562f44381de69ed79c5cb76f992c
Shi Jin <jinzishuai@gmail.com>
2020-08-22 13:37:34 -0600

    add test

--- test/testPublic.sh
+++ test/testPublic.sh
@@ -6,6 +6,7 @@ SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
 TOPFOLDER=$(dirname $SCRIPTPATH)
 
+apt install -y jq
 pushd $TOPFOLDER
 
 python -m gtl2via -i test/defect_boxes.manifest -o test/via.json -l zerobox-quick-detection -s zerobox-public
@@ -14,9 +15,19 @@ if [[ $(cat test/via.json |wc -m) == 4403 ]]; then
     echo "Correct results"
     exit 0
 else
-    apt install -y jq
     echo "Wrong results:"    
     cat test/via.json | jq
+    exit 1
 fi
 
+python -m viapl2rect -i test/polyline.json -o test/rect.json
+
+if [[ $(cat test/rect.json |jq |grep rect|wc -l) == 2 ]]; then
+    echo "Correct results"
+    exit 0
+else
+    echo "Wrong results: expected to find two rect"    
+    cat test/rect.json |jq
+    exit 2
+fi
 popd

3cc7607e205206655542f59528b9faf51d633b7b
Shi Jin <jinzishuai@gmail.com>
2020-08-23 17:40:32 -0600

    remove early return condition of testConverters.shh

--- test/testConverters.sh
+++ test/testConverters.sh
@@ -9,7 +9,6 @@ TOPFOLDER=$(dirname $SCRIPTPATH)
 pushd $TOPFOLDER
 
 python -m gtl2via -i test/defect_boxes.manifest -o test/via.json -l zerobox-quick-detection -s zerobox-public
-
 if [[ $(cat test/via.json |wc -m) == 4403 ]]; then
     echo "Correct results with gtl2via. Continue"
 else
@@ -19,10 +18,8 @@ else
 fi
 
 python -m viapl2rect -i test/polyline.json -o test/rect.json
-
 if [[ $(cat test/rect.json | jq '.' | grep rect | wc -l) == 2 ]]; then
     echo "Correct results with viapl2rect. Continue"
-    exit 0
 else
     echo "Wrong results: expected to find two rect:" 
     cat test/rect.json | jq '.'
@@ -30,7 +27,7 @@ else
 fi
 
 python -m cocolabels2json -o test/cvat.json -i 41
-if [[ $(cat cvat.json |jq '.' |grep name |wc) == 42 ]]; then
+if [[ $(cat test/cvat.json |jq '.' |grep name |wc -l) == 42 ]]; then
     echo "Correct results with cocolabels2json"
     exit 0
 else
